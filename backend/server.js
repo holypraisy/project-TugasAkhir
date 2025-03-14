6e191934d0ff98fdd654464be2f1984b9612b9b0
hollypraisy <lapianholly@gmail.com>
2025-03-14 10:51:24 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const moogoose = require('mongoose');
+const cookieParser = require ('cookie-parser');
+const cors = require('cors');
+
+moogoose
+    .connect('mongodb+srv://lapianholly:dataBase_projectTA@cluster0.uwl3o.mongodb.net/')
+    .then (() => console.log("connected"))
+    .catch((error) => console.log(error));
+
+const app =  express()
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173/' ,
+        methods : ['GET', 'POST', 'DELETE' , 'PUT'],
+        allowedHeaders : ['Content-Type', 'Authorization' , 'Cache-Control' , 'Expires' , 'Pragma'],
+        credentials : true
+})
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log (`on port ${PORT}`));
