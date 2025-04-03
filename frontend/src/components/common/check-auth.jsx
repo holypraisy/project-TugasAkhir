c1c5ba28f939d114f568d149962c2360b076a364
hollypraisy <lapianholly@gmail.com>
2025-04-04 02:45:59 +0800

    auth, and form

--- frontend/src/components/common/check-auth.jsx
+++ frontend/src/components/common/check-auth.jsx
@@ -1,36 +1,61 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from "react-router-dom";
 
+function CheckAuth({ isAuthenticated, user, children }) {
+  const location = useLocation();
 
-function CheckAuth(isAuthenticated, user, children) {
-    const location = useLocation()
+  console.log(location.pathname, isAuthenticated);
 
-    if(!isAuthenticated 
-      && !(
-            location.pathname.includes('/login') || 
-            location.pathname.includes('/register')
-          ) 
-        ) {
-          return <Navigate to = "/auth/login"/>;
-        }
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
+      return <Navigate to="/auth/login" />;
+    } else {
+      if (user?.role === "admin") {
+        return <Navigate to="/admin/dashboard" />;
+      } else {
+        return <Navigate to="/shop/home" />;
+      }
+    }
+  }
 
-        if(isAuthenticated && (location.pathname.includes('/login') ||
-        location.pathname.includes('/register'))) {
-          if(user?.role === 'admin'){
-            return <Navigate to = "/admin/dashboard"/>;
-          } else {
-            return <Navigate to = "/shop/home"/>;
-          }
-        }
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
 
-        if(isAuthenticated && user?.role !== 'admin' && location.pathname.includes(admin)) {
-          return <Navigate to = "/unauth-page"/>;
-        }
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
 
-        if(isAuthenticated && user?.role === 'admin' && location.pathname.includes('shop')){
-          return <Navigate to="/admin/dashboard"/>;
-        }
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
 
-        return  <>{children}</>
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  return <>{children}</>;
 }
 
-export default CheckAuth
+export default CheckAuth;
---
+++ frontend/src/components/common/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CommonForm() {
+  return (
+    <div>
+      <form action="">
+        
+      </form>
+    </div>
+  )
+}
+
+export default CommonForm
